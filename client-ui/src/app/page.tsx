fdf424c28cb7627a5bd93985c301ae4dcee79570
Christopher Connolly <425070+chaosloth@users.noreply.github.com>
2024-06-10 13:27:49 +0800

    Dynamic ID

--- client-ui/src/app/page.tsx
+++ client-ui/src/app/page.tsx
@@ -4,6 +4,7 @@ import { Call, Device } from "@twilio/voice-sdk";
 import { CustomizationProvider } from "@twilio-paste/core/customization";
 import { Alert, Button } from "@twilio-paste/core";
 import { useEffect, useState } from "react";
+import { generateSlug } from "random-word-slugs";
 
 import { Phase } from "@/types/Phases";
 
@@ -30,9 +31,12 @@ export default function Home() {
   useEffect(() => {
     console.log("Init Voice Service");
     setStatusText("Initializing");
+
+    // Get client ID from local storage or generate new one
+    let defaultUserId = localStorage.getItem("userId") || generateSlug();
+    localStorage.setItem("userId", defaultUserId);
     // Parse user_id query parameter (optional)
     const searchParams = new URLSearchParams(document.location.search);
-    let defaultUserId = "demo";
     if (searchParams.get("user_id")) {
       defaultUserId = searchParams.get("user_id") || "demo";
     }
@@ -187,11 +191,14 @@ export default function Home() {
   };
 
   return (
-    <CustomizationProvider baseTheme="dark" theme={{
-      backgroundColors: {
-        colorBackgroundBody: "#1c052e",
-      }
-    }}>
+    <CustomizationProvider
+      baseTheme="dark"
+      theme={{
+        backgroundColors: {
+          colorBackgroundBody: "#1c052e",
+        },
+      }}
+    >
       <CenterLayout>
         {hasPermissionError && (
           <Alert variant="error">
